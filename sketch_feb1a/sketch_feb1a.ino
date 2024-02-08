61e0573800732fc71ed64de1f2ceaa47d0db10a3
CalebMartin19067 <158525078+CalebMartin19067@users.noreply.github.com>
2024-02-07 17:54:04 -0800

    Code, 8th of February

--- sketch_feb1a/sketch_feb1a.ino
+++ sketch_feb1a/sketch_feb1a.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+# 19067
+//setting up pins
+const byte LEDPIN = 13 const byte PAWPIN1 = 1;
+const byte PAWPIN2 = 2;
+const byte PAWPIN3 = 3;
+const byte PAWPIN4 = 4;
 
+//setting pins as inputs and outputs
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(PAWPIN1, INPUT;
+  pinMode(PAWPIN2, INPUT);
+  pinMode(PAWPIN3, INPUT);
+  pinMode(PAWPIN4, INPUT);
 }
+// put your setup code here, to run once:
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-foobar
+  if (digitalRead(LEDPIN) == HIGH) {
+    digitalWrite(LEDPIN, LOW);
+  }
+  if (digitalRead(PAWPIN1) == HIGH) {
+    digitalWrite(LEDPIN, HIGH);
+  }
+  if (digitalRead(PAWPIN2) == HIGH) {
+    digitalWrite(LEDPIN, HIGH);
+  }
+  if (digitalRead(PAWPIN3) == HIGH) {
+    digitalWrite(LEDPIN, HIGH);
+  }
+  if (digitalRead(PAWPIN4) == HIGH) {
+    int score = playGame();
+  }
+}
+
+void playMusic(){
+  
+}
+int playGame(){
+  byte score = 99;
+  return score;
 }
