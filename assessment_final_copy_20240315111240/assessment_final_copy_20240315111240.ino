8cee1f319cef69df253b8c32dace8fa0a2059368
CalebMartin19067 <158525078+CalebMartin19067@users.noreply.github.com>
2024-03-15 11:17:25 +1300

    file added
    add

---
+++ assessment_final_copy_20240315111240/assessment_final_copy_20240315111240.ino
@@ -0,0 +1,37 @@
+
+#include <Wire.h>
+#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+
+const String FILENAME = "jokes.txt";
+OpenLog sdCard;  //Create an instance of the openlog board
+
+void setup() {
+
+  Wire.begin();
+  sdCard.begin();
+  sdCard.create(FILENAME);
+  sdCard.append(FILENAME); //Upload all further sdCard print statements to this file 
+  sdCard.println("knock knock");
+  sdCard.syncFile(); //buffer synced to the sdCard
+  Serial.begin(9600);
+  Serial.println("sdCard should have a file now, knock knock");
+}
+
+void loop() {
+  sdCard.println("Knock knock");
+  Serial.println("Knock knock");
+
+  sdCard.println("Who's there?");
+  Serial.println("Who's there?");
+
+  sdCard.println("South african wimpering");
+  Serial.println("South african wimpering");
+
+  sdCard.println("South african wimpering who?");
+  Serial.println("South african wimpering who?");
+
+  sdCard.println("Justin");
+  Serial.println("Justin");
+  sdCard.syncFile();
+}
+
--- code_feb8th.ino/code_feb8th.ino.ino
+++ code_feb8th.ino/code_feb8th.ino.ino
@@ -1,18 +1,28 @@
+#include <Servo.h>
+
 //shared with Mr Hansen successfully :)
 // 19067
 //setting up pins
-const byte LEDPIN = 13 const byte PAWPIN1 = 1;
+const byte LEDPIN = 13;
+const byte PAWPIN1 = 1;
 const byte PAWPIN2 = 2;
 const byte PAWPIN3 = 3;
 const byte PAWPIN4 = 4;
+const byte LEFTEARPIN = 9;
+const byte RIGHTEARPIN = 10;
+
+Servo leftEar;
+Servo rightEar;
 
 //setting pins as inputs and outputs
 void setup() {
   pinMode(13, OUTPUT);
-  pinMode(PAWPIN1, INPUT;
+  pinMode(PAWPIN1, INPUT);
   pinMode(PAWPIN2, INPUT);
   pinMode(PAWPIN3, INPUT);
   pinMode(PAWPIN4, INPUT);
+  leftEar.attach(LEFTEARPIN);
+  rightEar.attach(RIGHTEARPIN);
 }
 // put your setup code here, to run once:
 
@@ -35,10 +45,9 @@ void loop() {
   }
 }
 
-void playMusic(){
-  
+void playMusic() {
 }
-int playGame(){
+int playGame() {
   byte score = 99;
   return score;
 }
