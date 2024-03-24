ffeae63838e5dce566643fa2861603bc658e81d0
CalebMartin19067 <158525078+CalebMartin19067@users.noreply.github.com>
2024-03-25 10:14:38 +1300

    Add files via upload
    Second iteration of working code

---
+++ assessment_final_copy_20240315111240/iteration2finalcode25.03.24.ino
@@ -0,0 +1,55 @@
+//19067
+#include <Wire.h>
+#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+#include <SparkFun_LPS25HB_Arduino_Library.h>
+
+LPS25HB pressureSensor;
+
+const String DATAFILE = "calebdata.txt";
+OpenLog sdCard;  //Create an instance of the openlog board
+
+void setup() {
+
+  Wire.begin();
+  sdCard.begin();
+  sdCard.create(DATAFILE);
+  sdCard.append(DATAFILE);  //Upload all further sdCard print statements to this file
+  sdCard.println("Data file created");
+  sdCard.syncFile();  //buffer synced to the sdCard
+  Serial.begin(9600);
+  Serial.println("<LPS25HB Pressure Sensor Code>");
+  Serial.println("sdCard should have a data-file now");
+
+  if (pressureSensor.isConnected() == true)
+  {
+  Serial.println("LPS25HB Connected");
+  }
+ 
+  if (pressureSensor.isConnected() == false)  // if the pressure module is not connected, create error "DISCONNECTED"
+  {
+    Serial.println("LPS25HB disconnected. Reset the board to try again.");      // Alert the user that the module isnt connected
+    Serial.println("Are you using the right Wire port and I2C address?");       // Suggest possible fixes
+    Serial.println("See Example2_I2C_Configuration for how to change these.");  // Suggest possible fixes
+    Serial.println("");
+    sdCard.println("Recorded Data;");
+    //while (1);
+
+  }
+
+
+}
+
+void loop() {
+
+  Serial.print("Pressure in hPa:");
+  sdCard.println("Pressure in hPa:");
+  Serial.print(pressureSensor.getPressure_hPa());  // Get the pressure reading in hPa
+  sdCard.print(pressureSensor.getPressure_hPa());  // Get the pressure reading in hPa
+  Serial.print(", Temperature (degC): ");
+  sdCard.print(",");
+  Serial.println(pressureSensor.getTemperature_degC());  // Get the temperature in degrees C
+  sdCard.println(pressureSensor.getTemperature_degC());
+  sdCard.syncFile();
+
+  delay(40);  // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+}
